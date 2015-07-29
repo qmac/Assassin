072570120425ac74f1d0d81d3aa3f21624e071d3
Austin Tsao <atsao72@gmail.com>
2015-07-29 10:27:05 -0500

    Add login and signup views

---
+++ AssassinApp/AssassinApp/SSNLogInViewController.h
@@ -0,0 +1,14 @@
+//
+//  SSNLogInViewController.h
+//  AssassinApp
+//
+//  Created by Austin Tsao on 7/29/15.
+//  Copyright (c) 2015 Quinn McNamara. All rights reserved.
+//
+
+#import <ParseUI/ParseUI.h>
+#import <ParseUI/PFLogInViewController.h>
+
+@interface SSNLogInViewController : PFLogInViewController
+
+@end
---
+++ AssassinApp/AssassinApp/SSNSignUpViewController.h
@@ -0,0 +1,13 @@
+//
+//  SSNSignUpViewController.h
+//  AssassinApp
+//
+//  Created by Austin Tsao on 7/29/15.
+//  Copyright (c) 2015 Quinn McNamara. All rights reserved.
+//
+
+#import <ParseUI/ParseUI.h>
+
+@interface SSNSignUpViewController : PFSignUpViewController
+
+@end
