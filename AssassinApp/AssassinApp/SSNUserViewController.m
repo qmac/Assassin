f0f1a4a8f7c5f8c9824f619f2b9eef9942997eb6
Quinn McNamara <quinn.mcnamara@hudl.com>
2015-07-29 13:40:02 -0500

    SSN-3: added stubs for datasource and delegate

--- AssassinApp/AssassinApp/SSNUserViewController.m
+++ AssassinApp/AssassinApp/SSNUserViewController.m
@@ -8,30 +8,47 @@
 
 #import "SSNUserViewController.h"
 
-@interface SSNUserViewController ()
+@interface SSNUserViewController () <UITableViewDataSource, UITableViewDelegate>
+
+@property (nonatomic, strong, readwrite) UITableView *tableView;
 
 @end
 
 @implementation SSNUserViewController
 
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
     [super viewDidLoad];
-    // Do any additional setup after loading the view from its nib.
+    
+    self.tableView.dataSource = self;
+    self.tableView.delegate = self;
 }
 
-- (void)didReceiveMemoryWarning {
+- (void)didReceiveMemoryWarning
+{
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
 
-/*
-#pragma mark - Navigation
+#pragma mark UITableView Datasource
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = nil;
+    return cell;
 }
-*/
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 0;
+}
+
+#pragma mark UITableView Delegate
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Handle when user taps on a cell
+}
+
 
 @end
