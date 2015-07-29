eb28db6d8b8d1081e79e80d63dced70061a6a3de
Quinn McNamara <quinn.mcnamara@hudl.com>
2015-07-29 09:17:10 -0500

    added appid and client key for parse

--- AssassinApp/AssassinApp/AppDelegate.m
+++ AssassinApp/AssassinApp/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "Parse/Parse.h"
 
 @interface AppDelegate ()
 
@@ -17,6 +18,9 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    
+    [Parse setApplicationId:@"u9m11ErRytB4i6hNRUNvBMBeROirhXRp93Zj5oKY"
+                  clientKey:@"6KeMZ2zHH1wPXW5zv6isZqpyG08jX0TRh3iG3CEG"];
     return YES;
 }
 
