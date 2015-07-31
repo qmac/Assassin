ad5e5b0792ee91de65062ddb43d5b1d70b154237
Quinn McNamara <quinn.mcnamara@hudl.com>
2015-07-30 20:56:55 -0500

    added zoom on load

--- AssassinApp/AssassinApp/SSNLastSeenViewController.m
+++ AssassinApp/AssassinApp/SSNLastSeenViewController.m
@@ -14,17 +14,28 @@
 
 @implementation SSNLastSeenViewController
 
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
     [super viewDidLoad];
     
     CLLocationCoordinate2D coordinate = CLLocationCoordinate2DMake(self.targetPoint.latitude, self.targetPoint.longitude);
     MKPlacemark *targetPlacemark = [[MKPlacemark alloc] initWithCoordinate:coordinate addressDictionary:nil];
     [self.mapView addAnnotation:targetPlacemark];
+    
+    [self zoomToCoordinate:coordinate];
 }
 
-- (void)didReceiveMemoryWarning {
+- (void)didReceiveMemoryWarning
+{
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+-(void)zoomToCoordinate:(CLLocationCoordinate2D) coordinate
+{
+    MKCoordinateRegion region = { {0.0, 0.0 }, { 0.0, 0.0 } };
+    region.center = coordinate;
+    [self.mapView setRegion:region animated:YES];
 }
 
 @end
